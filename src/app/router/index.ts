f0b84186ae6908d843ae89ffa865f1a77c0c4e6f
Rafi Ferdos <rafiferdos@gmail.com>
2025-02-16 00:07:52 +0600

    add user and transaction routes to the main router

--- src/app/router/index.ts
+++ src/app/router/index.ts
@@ -1,8 +1,19 @@
 import { Router } from 'express'
+import RUser from '../modules/Auth/auth.route'
+import RTransaction from '../modules/Transaction/transaction.route'
 
 const router = Router()
 
-const routers = []
+const routers = [
+  {
+    path: '/',
+    router: RUser,
+  },
+  {
+    path: '/transaction',
+    router: RTransaction,
+  },
+]
 
 routers.forEach((route) => router.use(route.path, route.router))
 
